f124cd03b02e5d7b9e115ced130b94f1b71bfd4f
Grigor Sargsyan <grigor.sargsyan@yahoo.com>
2020-05-03 14:27:27 -0700

    Day #3

---
+++ Ransom Note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> letterPool = new HashMap<Character, Integer>();
+        
+        for(int i = 0; i < magazine.length(); i++){
+            char c = magazine.charAt(i);
+            letterPool.put(c, letterPool.getOrDefault(c, 0) + 1);
+        }
+        
+        for(int i = 0; i < ransomNote.length(); i++){
+            char c = ransomNote.charAt(i);
+            if(letterPool.containsKey(c) && letterPool.get(c) > 0){
+                letterPool.replace(c, letterPool.get(c) - 1);
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+}
